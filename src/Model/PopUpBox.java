6bfa08fd879743a833022f89c17c9423028e218c
Cody <Sharpeleven91@gmail.com>
2022-08-15 19:45:34 -0700

    add user and userDao classes

---
+++ src/Model/PopUpBox.java
@@ -0,0 +1,24 @@
+package Model;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
+
+public class PopUpBox {
+    public static void displayError(String message) {
+
+        ButtonType okButton = new ButtonType(Translator.getTranslation("ok"), ButtonBar.ButtonData.CANCEL_CLOSE);
+        Alert alert = new Alert(Alert.AlertType.WARNING, "", okButton);
+        alert.setHeaderText(Translator.getTranslation("warning"));
+        alert.setTitle(Translator.getTranslation("warning"));
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
+    private void displayConfirmation(String message) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle(Translator.getTranslation("confirmation"));
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+}
