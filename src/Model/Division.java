fa24434c1eb873002314f1ea174d3852c26c8e91
Cody <Sharpeleven91@gmail.com>
2022-08-24 07:49:11 -0700

    add division documentation

--- src/Model/Division.java
+++ src/Model/Division.java
@@ -1,18 +1,44 @@
 package Model;
 
+/**
+ * represents a Division
+ */
 public class Division {
+    /** division id **/
     int id;
+    /** division name **/
     String name;
+    /** division's country id **/
     int countryId;
 
+    /**
+     * @param id division id
+     * @param name division name
+     * @param countryId division country id
+     */
     public Division(int id, String name, int countryId) {
         this.id = id;
         this.name = name;
         this.countryId = countryId;
     }
 
+    /**
+     * @return division id
+     */
     public int getId() { return this.id; }
+
+    /**
+     * @return division name
+     */
     public String getName() { return this.name; }
+
+    /**
+     * @return division country id
+     */
     public int getCountryId() { return this.countryId; }
+
+    /**
+     * @return division name
+     */
     public String toString() { return this.name; }
 }
