c23527b01c96bc48ba982253f73b8d691cc59fa7
Cody <Sharpeleven91@gmail.com>
2022-08-24 07:47:27 -0700

    add documentation to customer

--- src/Model/Customer.java
+++ src/Model/Customer.java
@@ -1,14 +1,29 @@
 package Model;
 
+/** represents a Customer **/
 public class Customer {
 
+    /** customer id **/
     private int id;
+    /** customer name */
     private String name;
+    /** customer address **/
     private String address;
+    /** customer postal code **/
     private String postalCode;
+    /** customer phone **/
     private String phone;
+    /** customer division id **/
     private int divisionId;
 
+    /**
+     * @param id customer id
+     * @param name customer name
+     * @param address customer address
+     * @param postalCode customer postal code
+     * @param phone customer phone
+     * @param divisionId customer division id
+     */
     public Customer(int id, String name, String address, String postalCode, String phone, int divisionId) {
         this.id = id;
         this.name = name;
@@ -18,22 +33,33 @@ public class Customer {
         this.divisionId = divisionId;
     }
 
+    /**
+     * @return customer id
+     */
     public int getId() { return this.id; }
-    public String getName() {
-        return this.name;
-    }
 
-    public String getAddress() {
-        return this.address;
-    }
+    /**
+     * @return customer name
+     */
+    public String getName() { return this.name; }
 
-    public String getPostalCode() {
-        return this.postalCode;
-    }
+    /**
+     * @return customer address
+     */
+    public String getAddress() { return this.address; }
 
-    public String getPhone() {
-        return this.phone;
-    }
+    /**
+     * @return customer postal code
+     */
+    public String getPostalCode() { return this.postalCode; }
 
+    /**
+     * @return customer phone number
+     */
+    public String getPhone() { return this.phone; }
+
+    /**
+     * @return customer division id
+     */
     public int getDivisionId() { return this.divisionId; }
 }
