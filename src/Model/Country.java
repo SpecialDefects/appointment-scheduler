0d50a74ba1d2f7f7bc6227fec3c5c4832f042b84
Cody <Sharpeleven91@gmail.com>
2022-08-24 07:44:21 -0700

    add Country documentation

--- src/Model/Country.java
+++ src/Model/Country.java
@@ -1,17 +1,36 @@
 package Model;
 
+/** represents a country **/
 public class Country {
+    /** country id **/
     int id;
+    /** country name **/
     String name;
 
+    /**
+     * @param id country id
+     * @param name country name
+     */
     public Country(int id, String name) {
         this.id = id;
         this.name = name;
     }
 
+    /**
+     *
+     * @return country id
+     */
     public int getId() { return this.id; }
 
+    /**
+     *
+     * @return country name
+     */
     public String getName() { return this.name; };
 
+    /**
+     *
+     * @return country name
+     */
     public String toString() { return this.name; };
 }
