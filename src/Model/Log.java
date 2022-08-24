ee428ba3967c7b3f4f590469388c509a2afb5de1
Cody <Sharpeleven91@gmail.com>
2022-08-24 07:59:58 -0700

    add documentation to Log

--- src/Model/Log.java
+++ src/Model/Log.java
@@ -4,15 +4,23 @@ import java.io.File;
 import java.io.FileWriter;
 import java.time.LocalDateTime;
 
+/** represents a logger for login actions **/
 public class Log {
 
+    /**
+     * records a login attempt associated to username
+     * @param username username used to attempt login
+     * @throws Exception
+     */
     public static void loginAttempt(String username) throws Exception {
+        /** create file **/
         try {
             File logFile = new File("./login_activity.txt");
             logFile.createNewFile();
         } catch(Exception e) {
             throw new Exception("Cannot create log file");
         }
+        /** write login attempt to file **/
         try {
             FileWriter writer = new FileWriter("./login_activity.txt");
             LocalDateTime now = LocalDateTime.now();
@@ -24,13 +32,20 @@ public class Log {
         }
     }
 
+    /**
+     * records a successful login associated to username
+     * @param username username used to login
+     * @throws Exception
+     */
     public static void loginSuccess(String username) throws Exception {
+        /** create file **/
         try {
             File logFile = new File("../login_activity.txt");
             logFile.createNewFile();
         } catch(Exception e) {
             throw new Exception("Cannot create log file");
         }
+        /** write successful user login to file **/
         try {
             FileWriter writer = new FileWriter("../login_activity.txt");
             LocalDateTime now = LocalDateTime.now();
