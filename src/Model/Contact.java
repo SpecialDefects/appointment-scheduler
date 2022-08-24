70257fedbb398464dd12c7ebf044238e4f6e49d9
Cody <Sharpeleven91@gmail.com>
2022-08-24 07:43:05 -0700

    add Contact documentation **/

--- src/Model/Contact.java
+++ src/Model/Contact.java
@@ -1,18 +1,49 @@
 package Model;
 
+/**
+ * represents a Contact
+ */
 public class Contact {
+    /** contact id **/
     int id;
+    /** contact name **/
     String name;
+    /** contact email **/
     String email;
 
+    /**
+     *
+     * @param id contact id
+     * @param name contact name
+     * @param email contact email
+     */
     public Contact(int id, String name, String email) {
         this.id = id;
         this.name = name;
         this.email = email;
     }
 
+    /**
+     *
+     * @return contact id
+     */
     public int getId() { return this.id; }
+
+    /**
+     *
+     * @return contact name
+     */
     public String getName() { return this.name; }
+
+    /**
+     *
+     * @return contact email
+     */
     public String getEmail() { return this.email; }
+
+    /**
+     *
+     * @return contact name
+     */
     public String toString() { return this.name; }
 }
